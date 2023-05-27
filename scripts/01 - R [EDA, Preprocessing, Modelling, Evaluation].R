e6c7cf12e0383af3433f7039cb5de5c403e8b602
simonweigold <simonw750@gmail.com>
2023-05-27 21:13:05 +0200

    added pre-processing.

--- scripts/01 - R [EDA, Preprocessing, Modelling, Evaluation].R
+++ scripts/01 - R [EDA, Preprocessing, Modelling, Evaluation].R
@@ -10,4 +10,14 @@ train <- rio::import(here::here("data", "train.csv"))
 test <- rio::import(here::here("data", "test.csv"))
 
 
-
+# Pre-processing ----------------------------------------------------------
+clean_train <- separate(train, col = Cabin,
+                        into = c("Cabin_deck", "Cabin_num", "Cabin_side"),
+                        sep = "/") #categorise cabin position
+clean_train$PassengerId = substr(clean_train$PassengerId, start = 1, stop = 4) #define whether passenger is alone or in a group
+clean_train$group<- ifelse(duplicated(clean_train$PassengerId) |
+                             duplicated(clean_train$PassengerId, fromLast = TRUE),
+                           "Multiple", "Unique")
+clean_train <- clean_train %>% select(-c(PassengerId, Name)) #remove redundant variables
+clean_train <- clean_train %>% mutate_all(~ifelse(. == "", NA, .)) #define empty entries as NA
+clean_train <- clean_train %>% drop_na() #remove NAs
