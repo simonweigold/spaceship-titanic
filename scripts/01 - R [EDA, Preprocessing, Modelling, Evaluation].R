a5665d29de534c0e8a63be48a3b2caea91858862
simonweigold <simonw750@gmail.com>
2023-06-19 17:15:19 +0200

    added parallelisation and neural network.

--- scripts/01 - R [EDA, Preprocessing, Modelling, Evaluation].R
+++ scripts/01 - R [EDA, Preprocessing, Modelling, Evaluation].R
@@ -4,6 +4,16 @@ library(here)
 library(rio)
 library(tidymodels)
 library(corrplot)
+library(tune)
+library(scales)
+library(parallel)
+library(doParallel)
+
+
+# Start parallelisation ---------------------------------------------------
+cores <- parallel::detectCores(logical = FALSE)
+cl <- parallel::makePSOCKcluster(cores)
+doParallel::registerDoParallel(cl)
 
 
 # Data import -------------------------------------------------------------
@@ -16,6 +26,7 @@ test <- rio::import(here::here("data", "test.csv"))
 source(here::here("scripts", "R data preprocessing function.R"))
 # Train data
 clean_train <- clean_data(train)
+clean_train$Transported <- as.character(clean_train$Transported) # Transported only exists in Train!
 # Test data
 clean_test <- clean_data(test)
 
@@ -78,7 +89,9 @@ train_rec <-recipe(Transported ~., data = train_data) %>%
 train_folds <- vfold_cv(data = train_data, v = 5) #cross validation
 
 
-# Specify model type and computational engine -----------------------------
+
+# GBT model ---------------------------------------------------------------
+# Specify model type and computational engine
 gbt_model <-
   boost_tree()%>% #Model type: Gradient Boosted Tree
   set_engine("xgboost")%>% # omputational engine: xgboost
@@ -88,20 +101,20 @@ gbt_model <-
            tree_depth = 6, min_n = 1)
 
 
-# Create a regular tune grid ----------------------------------------------
+# Create a regular tune grid
 gbt_grid <- grid_regular(range_set(trees(), c(300,600)),
                          range_set(learn_rate(trans = NULL), c(0.005, 0.02)),
                          levels = 4)
 
 
-# Create workflow ---------------------------------------------------------
+# Create workflow
 gbt_wflow <- 
   workflow() %>% 
   add_model(gbt_model) %>%
   add_recipe(train_rec)
 
 
-# Analyse resamples -------------------------------------------------------
+# Analyse resamples
 system.time(
   gbt_res <- 
     gbt_wflow %>% 
@@ -111,12 +124,12 @@ system.time(
 )
 
 
-# Collect metrics ---------------------------------------------------------
+# Collect metrics
 collect_metrics(gbt_res)
 autoplot(gbt_res)
 
 
-# Finalise model workflow -------------------------------------------------
+# Finalise model workflow
 best_gbt <- select_best(x = gbt_res)
 
 final_wf <- 
@@ -124,7 +137,7 @@ final_wf <-
   finalize_workflow(best_gbt)
 
 
-# Fit and predict ---------------------------------------------------------
+# Fit and predict
 final_gbt <- fit(final_wf, train_data)
 
 gbt_pred <- predict(final_gbt, new_data = test_data)
@@ -133,8 +146,76 @@ predictions <- test_data %>%
   bind_cols(., gbt_pred)
 
 
-# Check results -----------------------------------------------------------
+# Check results
 predictions$Transported <- as.factor(predictions$Transported)
 metrics(predictions, truth = Transported, estimate = .pred_class)
 conf_mat(predictions, truth = Transported, estimate = .pred_class)
 
+
+
+# Neural network ----------------------------------------------------------
+# Specify model type and computational engine
+nn_model <-
+  mlp() %>% # defines a multilayer perceptron model
+  set_engine("nnet") %>% # Computational engine: nnet
+  set_mode("classification") %>% # Specify model mode
+  set_args(hidden_units = tune(), penalty = tune(), epochs = tune())
+
+
+# Create a regular tuning grid
+nn_grid <- grid_random(range_set(hidden_units(), c(5, 15)),
+                       range_set(penalty(trans = NULL), c(0, 1)),
+                       range_set(epochs(), c(100, 500)),
+                       size = 10)
+
+# Create workflow
+nn_wflow <- 
+  workflow() %>% 
+  add_model(nn_model) %>%
+  add_recipe(train_rec)
+
+
+# Analyse resamples
+system.time(
+  nn_res <- 
+    nn_wflow %>% 
+    tune_grid(resamples = train_folds, grid = nn_grid,
+              #metrics = metric_set(huber_loss),
+              control = control_grid(verbose = T))
+)
+
+
+# Collect metrics
+collect_metrics(nn_res)
+autoplot(nn_res)
+show_best(x = nn_res)
+
+
+# Finalise model workflow
+best_nn <- select_best(x = nn_res)
+
+final_wf <- 
+  nn_wflow %>% 
+  finalize_workflow(best_nn)
+
+
+# Fit and predict
+final_nn <- fit(final_wf, train_data)
+
+nn_pred <- predict(final_nn, new_data = test_data)
+
+predictions <- test_data %>% 
+  select(Transported) %>% # keep target variable (also known as the truth)
+  bind_cols(., nn_pred)
+
+
+# Calculate performance metrics
+predictions$Transported <- as.factor(predictions$Transported)
+metrics(predictions, truth = Transported, estimate = .pred_class)
+conf_mat(predictions, truth = Transported, estimate = .pred_class)
+
+
+# End parallelisation -----------------------------------------------------
+parallel::stopCluster(cl)
+closeAllConnections()
+showConnections()
