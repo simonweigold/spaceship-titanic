56436ba19088256196bcd11673106431e15bab4c
simonweigold <simonw750@gmail.com>
2023-06-19 15:25:17 +0200

    new R script with function which pre-processes data into the right format.

---
+++ scripts/R data preprocessing function.R
@@ -0,0 +1,32 @@
+clean_data <- function(data) {
+  library(dplyr)
+  library(tidyr)
+  
+  # Separate Cabin column into three variables
+  data <- separate(data, col = Cabin, into = c("Cabin_deck", "Cabin_num", "Cabin_side"), sep = "/")
+  
+  # Categorize CryoSleep, VIP, and Transported variables as characters
+  data$CryoSleep <- as.character(data$CryoSleep)
+  data$VIP <- as.character(data$VIP)
+  data$Transported <- as.character(data$Transported)
+  
+  # Convert Cabin_num to numeric
+  data$Cabin_num <- as.numeric(data$Cabin_num)
+  
+  # Define whether passenger is alone or in a group based on PassengerId
+  data$PassengerId <- substr(data$PassengerId, start = 1, stop = 4)
+  data$group <- ifelse(duplicated(data$PassengerId) | duplicated(data$PassengerId, fromLast = TRUE),
+                       "Multiple", "Unique")
+  
+  # Remove redundant variables
+  data <- select(data, -c(PassengerId, Name))
+  
+  # Define empty entries as NA
+  data <- mutate_all(data, ~ifelse(. == "", NA, .))
+  
+  # Remove rows with NAs
+  data <- drop_na(data)
+  
+  return(data)
+}
+
