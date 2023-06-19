c6610d96810c928a13654621fab1da818a4c91bd
simonweigold <simonw750@gmail.com>
2023-06-19 17:36:54 +0200

    engineered NA values so they are not dropped anymore.

--- scripts/R data preprocessing function.R
+++ scripts/R data preprocessing function.R
@@ -23,8 +23,22 @@ clean_data <- function(data) {
   # Define empty entries as NA
   data <- mutate_all(data, ~ifelse(. == "", NA, .))
   
-  # Remove rows with NAs
-  data <- drop_na(data)
+  # Engineer NAs
+  #data$example[is.na(data$example)] <- median(data$example, na.rm = TRUE)
+  data$Cabin_num[is.na(data$Cabin_num)] <- median(data$Cabin_num, na.rm = TRUE)
+  data$Age[is.na(data$Age)] <- median(data$Age, na.rm = TRUE)
+  data$RoomService[is.na(data$RoomService)] <- median(data$RoomService, na.rm = TRUE)
+  data$FoodCourt[is.na(data$FoodCourt)] <- median(data$FoodCourt, na.rm = TRUE)
+  data$ShoppingMall[is.na(data$ShoppingMall)] <- median(data$ShoppingMall, na.rm = TRUE)
+  data$Spa[is.na(data$Spa)] <- median(data$Spa, na.rm = TRUE)
+  data$VRDeck[is.na(data$VRDeck)] <- median(data$VRDeck, na.rm = TRUE)
+  #data$example[is.na(data$example)] <- names(which.max(table(data$example)))
+  data$HomePlanet[is.na(data$HomePlanet)] <- names(which.max(table(data$HomePlanet)))
+  data$CryoSleep[is.na(data$CryoSleep)] <- names(which.max(table(data$CryoSleep)))
+  data$Cabin_deck[is.na(data$Cabin_deck)] <- names(which.max(table(data$Cabin_deck)))
+  data$Cabin_side[is.na(data$Cabin_side)] <- names(which.max(table(data$Cabin_side)))
+  data$Destination[is.na(data$Destination)] <- names(which.max(table(data$Destination)))
+  data$VIP[is.na(data$VIP)] <- names(which.max(table(data$VIP)))
   
   return(data)
 }
